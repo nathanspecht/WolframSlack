de290db8e35659a30e207299a9f05f31f26fa3c1
Nathan Specht <nathanspecht@gmail.com>
2015-11-08 17:13:53 -0800

    Can talk to wolfram alpha api

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -2,8 +2,8 @@ class Api::QuestionsController < ApplicationController
   def create
     query = slack_params[:text]
     request_url = "http://api.wolframalpha.com/v2/query?input=#{query}&appid=#{ENV['wolfram_api_id']}"
-    resp = Net::HTTP.post(URI.parse(request_url))
-    render text: resp.body.read
+    resp = Net::HTTP.get(URI.parse(request_url))
+    render text: resp
   end
 
   private
