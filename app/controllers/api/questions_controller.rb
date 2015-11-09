a99c9eff069ccd32f255aac88df7409f393f2702
Nathan Specht <nathanspecht@gmail.com>
2015-11-08 17:28:10 -0800

    parses wa response into three image tags

--- app/controllers/api/questions_controller.rb
+++ app/controllers/api/questions_controller.rb
@@ -3,7 +3,12 @@ class Api::QuestionsController < ApplicationController
     query = slack_params[:text]
     request_url = "http://api.wolframalpha.com/v2/query?input=#{query}&appid=#{ENV['wolfram_api_id']}"
     resp = Net::HTTP.get(URI.parse(request_url))
-    render text: resp
+
+    xml_resp = Nokogiri::XML(resp)
+    resp_imgs = xml_resp.xpath('//img')
+
+    #fetches the first three image results
+    render text: resp_imgs[0..2]
   end
 
   private
